3c507504277b2b5dbdc791032a399f4ab9711adc
wzc <1477099521@qq>
2024-02-27 15:57:34 +0800

    commit Idempotent Interface

---
+++ SpringBoot-Idempotent-Interface/src/main/java/com/wzc/SpringBootIdempotentInterfaceApplication.java
@@ -0,0 +1,12 @@
+package com.wzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIdempotentInterfaceApplication
+{
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIdempotentInterfaceApplication.class, args);
+    }
+}
