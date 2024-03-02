ff2f1978f978bf149a8fa5526c31fa54e449d406
wzc <1477099521@qq>
2024-03-02 17:01:36 +0800

    commit AliPay

---
+++ SpringBoot-AliPay/src/main/java/com/wzc/SpringBootAliPayApplication.java
@@ -0,0 +1,11 @@
+package com.wzc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootAliPayApplication
+{
+    public static void main( String[] args ) {
+        SpringApplication.run(SpringBootAliPayApplication.class, args);
+    }
+}
